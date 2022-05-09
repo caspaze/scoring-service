01a33b4a216e4c86f31e74e9599a7e56411e2dd2
vnovoselov <vadimnovoselov.me@gmail.com>
2022-05-09 16:27:48 +0300

    initial commit

---
+++ src/main/java/ru/vsu/scoringservice/ScoringServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.scoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoringServiceApplication.class, args);
+    }
+
+}
